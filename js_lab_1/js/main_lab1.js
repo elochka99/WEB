631ee42ada704fe9557395f8486a9bb8d6f269a1
Elena Gorokhova <elochka150899@gmail.com>
2019-02-13 23:01:20 +0200

    Add files via upload

--- js_lab_1/js/main_lab1.js
+++ js_lab_1/js/main_lab1.js
@@ -1,15 +1,21 @@
-var seconds = 234245645335;
-var time_ost = seconds%3600;
-console.log(seconds);
-console.log(time_ost);
+var num = 234245645335;
+var time_ost = num%3600;
+console.log(num);
+var hours = Math.floor( num/3600);
+console.log("часов - " + hours);
+var minutes = Math.floor(time_ost/60);
+console.log("минут - " + minutes);
+var seconds = time_ost%60;
+console.log("cекунд - " + seconds);
+// console.log(time_ost);
+console.log(hours + ' : ' + minutes + ' : ' + seconds);
+
 
 var symbols = [];
 var text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, at";
 
     for (var i = 0; i <= text.length-1; i++ ){
         console.log(text.indexOf("a",i));
-        // console.log(symbols.push(text.indexOf("a",i)));
-        // symbols[0]=[text.indexOf("a",i)];
         if (text.charAt(i) == "a"){
             symbols.push(i);
         }
