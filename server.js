59cb37f282e7e9ab391adf49b79227370566417d
Elena Gorokhova <elochka150899@gmail.com>
2019-04-09 20:41:57 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 
-var http = require('http');
- 
-http.createServer(function (request, response) {
-  response.end('Hello World\n');
-}).listen(3000, "127.0.0.1",function(){
-    console.log("Server running at http://localhost:3000/");
-});
+var express = require('express');
+var app = express();
+
+app.get("/vasia", function(req, res) {
+	res.status(200).send("Hello, world!");
+})
+
+app.get("/*", function(req, res) {
+	res.status(200).send(req.url);
+})
+
+
+app.listen('8000', 'localhost', function() {
+  console.log('listen on port 8000');
+})
+
+
  
